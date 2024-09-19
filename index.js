9d779bd3c21efd53d2c0f34d854cc176ccc8ab9e
Minentle Mvunyiswa <102150619+Minentle2301@users.noreply.github.com>
2024-09-19 18:51:28 +0200

    created bankbranchinstance variable

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 
 // 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
 
+let bankBranchInstance = null;
+
 // 2. Define a class called `BankBranch` for managing branch information.
+
 
 // 3. In the `BankBranch` class:
 //    - Create a constructor that takes `branchInfo` as a parameter.
+
 //    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
 //    - If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
 //    - Return the `bankBranchInstance` whether it's newly created or existing.
